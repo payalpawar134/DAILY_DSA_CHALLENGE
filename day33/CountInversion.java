9029df7dc804c4a4e6b5dcde3bbfe8d60bf48ab0
payalpawar134 <payalpawar134038@gmail.com>
2026-02-02 23:05:51 +0530

    day33

---
+++ day33/CountInversion.java
@@ -0,0 +1,16 @@
+class CountInversion {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
