a5b1e95060fb3d5890b935e1e0abbb3c0c780a54
payalpawar134 <payalpawar134038@gmail.com>
2026-02-05 23:05:35 +0530

    day36

---
+++ day36/FirstRepeating.java
@@ -0,0 +1,16 @@
+class FirstRepeating {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No repeating element");
+    }
+}
