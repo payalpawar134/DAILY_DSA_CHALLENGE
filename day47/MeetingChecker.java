18de67d62eedb4f66be17d98461fefa1cf877342
payalpawar134 <payalpawar134038@gmail.com>
2026-02-16 20:41:15 +0530

    day47

---
+++ day47/MeetingChecker.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class MeetingChecker {
+    
+    static boolean canAttend(int[][] arr) {
+        
+        
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            if (arr[i][0] < arr[i - 1][1]) {
+                return false; 
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        
+        int[][] arr = {
+            {1, 4},
+            {10, 15},
+            {7, 10}
+        };
+        
+        boolean result = canAttend(arr);
+        
+        System.out.println(result);
+    }
+}
