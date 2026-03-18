1d9905db737bd7b481396dfbefcdaaba3b4fbdac
payalpawar134 <payalpawar134038@gmail.com>
2026-03-18 21:11:29 +0530

    day70

---
+++ day70/SumArray2D.java
@@ -0,0 +1,20 @@
+class SumArray2D {
+    public static void main(String[] args) {
+        
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++) {          // rows
+            for(int j = 0; j < arr[i].length; j++) {   // columns
+                sum += arr[i][j];
+            }
+        }
+        
+        System.out.println("Sum = " + sum);
+    }
+}
